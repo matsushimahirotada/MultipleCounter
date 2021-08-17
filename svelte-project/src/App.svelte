5116fd470010a6085841bdef35da0473acb9e7b4
MatsushimaHirotada <t170d516@gunma-u.ac.jp>
2021-08-17 17:26:43 +0900

    カウント単体仮実装

--- svelte-project/src/App.svelte
+++ svelte-project/src/App.svelte
@@ -1,30 +1,40 @@
 <script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	let name = "new"; 
+	export let count = 0;
+	function ClickInc(){
+		count +=1
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+	function ClickDec(){
+		if (count>0){
+			count -=1
 		}
 	}
+
+	function Clickreset(){
+		count=0
+	}
+
+</script>
+
+<!-- svelte-ignore non-top-level-reactive-declaration -->
+<div>
+	<input bind:value={name}>
+
+	{count}
+	
+	<button on:click={ClickInc}>
+		+
+	</button>
+	
+	<button on:click={ClickDec}>
+		-
+	</button>
+	
+	<button on:click={Clickreset}>
+		0
+	</button>
+</div>
+
+<style>
 </style>
